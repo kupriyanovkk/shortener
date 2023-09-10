90eaff79af7525f52812867249bd4a02f61ee22e
Konstantin Kupriyanov <kupriyanovkk@gmail.com>
2023-09-10 16:21:09 +0300

    increment 3 is completed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/kupriyanovkk/shortener/internal/server"
-	"github.com/kupriyanovkk/shortener/internal/storage"
+	"github.com/kupriyanovkk/shortener/internal/app"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	var s = storage.NewStorage()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { server.HandleFunc(w, r, s) })
-
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
+	app.Start()
 }
