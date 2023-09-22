d13c8a5364a8a65d167dd93dbbf03dcf67080b0b
Konstantin Kupriyanov <kupriyanovkk@gmail.com>
2023-09-22 10:35:29 +0300

    increment 6 is completed

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/kupriyanovkk/shortener/internal/config"
+	"github.com/kupriyanovkk/shortener/internal/logger"
 	"github.com/kupriyanovkk/shortener/internal/server"
 	"github.com/kupriyanovkk/shortener/internal/storage"
 )
@@ -15,6 +16,7 @@ func Start() {
 	r := chi.NewRouter()
 	f := config.ParseFlags()
 
+	r.Use(logger.WithLogging)
 	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
 		server.GetHandler(w, r, s)
 	})
