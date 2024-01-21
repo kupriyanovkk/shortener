3e5db7d89f6e9f1d949b8399b48464f06380be69
Konstantin Kupriyanov <kupriyanovkk@gmail.com>
2024-01-21 16:39:03 +0300

    increment 20 is completed

--- internal/store/in_memory/store_test.go
+++ internal/store/in_memory/store_test.go
@@ -3,9 +3,11 @@ package inmemory
 import (
 	"context"
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/kupriyanovkk/shortener/internal/failure"
+	"github.com/kupriyanovkk/shortener/internal/models"
 	storeInterface "github.com/kupriyanovkk/shortener/internal/store/interface"
 )
 
@@ -108,6 +110,52 @@ func TestStore_GetValue(t *testing.T) {
 
 			if url != testCase.expectedURL {
 				t.Errorf("Expected URL: %s, but got URL: %s", testCase.expectedURL, url)
+			}
+		})
+	}
+}
+
+func TestStore_DeleteURLs(t *testing.T) {
+	ctx := context.Background()
+	s := Store{
+		values: map[string]models.URL{
+			"short1": {Short: "short1", Original: "original1", UserID: "user1"},
+			"short2": {Short: "short2", Original: "original2", UserID: "user2"},
+		},
+	}
+
+	tests := []struct {
+		name     string
+		opts     []storeInterface.DeletedURLs
+		expected map[string]models.URL
+	}{
+		{
+			name: "Mark URL as deleted for matching UserID and Short URL",
+			opts: []storeInterface.DeletedURLs{
+				{UserID: "user1", URLs: []string{"short1"}},
+			},
+			expected: map[string]models.URL{
+				"short1": {Short: "short1", Original: "original1", UserID: "user1", DeletedFlag: true},
+				"short2": {Short: "short2", Original: "original2", UserID: "user2"},
+			},
+		},
+		{
+			name: "No URLs to mark as deleted",
+			opts: []storeInterface.DeletedURLs{
+				{UserID: "user3", URLs: []string{"short3"}},
+			},
+			expected: map[string]models.URL{
+				"short1": {Short: "short1", Original: "original1", UserID: "user1", DeletedFlag: true},
+				"short2": {Short: "short2", Original: "original2", UserID: "user2"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s.DeleteURLs(ctx, tt.opts)
+			if !reflect.DeepEqual(s.values, tt.expected) {
+				t.Errorf("unexpected values map after %s test; got %v, want %v", tt.name, s.values, tt.expected)
 			}
 		})
 	}
